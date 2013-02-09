61a4aab7c0c686486da960aaf55f3940bccff196
Zhongkai Fu <fuzhongkai@gmail.com>
2013-02-09 10:32:10 +0800

    1. Add : add query term synonym pair command line tool

---
+++ src/QueryTermSynAnalyzerConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QueryTermSynonymAnalyzer;
+
+namespace QueryTermSynAnalyzerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryTermSynonymAnalyzer.QueryTermSynonymAnalyzer qts = new QueryTermSynonymAnalyzer.QueryTermSynonymAnalyzer("LexicalDict.txt", "syn_zh_cn.txt");
+
+            while (true)
+            {
+                string str = "顺丰物流电话";
+                Dictionary<string, List<string>> rstDict = qts.GetSynonym(str, 3, 2);
+            }
+
+        }
+    }
+}
